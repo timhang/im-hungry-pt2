e7200ee04a92b6785512dc97e5ad8a8e8f71c6cb
HaiyuTian <haiyutia@usc.edu>
2019-03-07 19:50:26 -0800

    recipeAPI testing updates

--- src/test/java/API/RecipeAPITest.java
+++ src/test/java/API/RecipeAPITest.java
@@ -11,37 +11,53 @@ public class RecipeAPITest {
 
 	@Test
 	public void testGetSetState() {
+		//test for the getter/setter of state
 		RecipeAPI testObj1 = new RecipeAPI();
 		testObj1.setState(false);
 		assertEquals("Mismatch of state", testObj1.getState(), false);
 	}
 
 	@Test
-	public void testReRank() {
-		
-		//the goal of this function is to rank the recipe based on prep time
+	public void testGetSetRecipeId() {
+		RecipeAPI testObj3 = new RecipeAPI();
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		arr.add(1);
+		testObj3.setRecipeId(arr);
+		assertEquals("Mismatch on recipe id", testObj3.getRecipeId(), arr);
+	}
+
+	@Test
+	public void testGetFavorites() {
+		//test two cases; true and false scenarios
 		ArrayList<Integer> recipes = new ArrayList<Integer>();
 		recipes.add(1);
 		recipes.add(2);
 		HashMap<Integer, Recipe> allRecipes = new HashMap<Integer, Recipe>();
 		Recipe recipeOne = new Recipe(1);
 		Recipe recipeTwo = new Recipe(2);
-		recipeOne.setPrepTime(20);
-		recipeTwo.setPrepTime(10);
+		recipeOne.setFavorite(true);
+		recipeTwo.setFavorite(false);
+		recipeOne.setDoNotShow(true);
+		recipeTwo.setDoNotShow(false);
+		recipeOne.setToExplore(true);
+		recipeTwo.setToExplore(false);
+		RecipeAPI testObjA = new RecipeAPI();
+		testObjA.setRecipeId(recipes);
+		testObjA.setRecipeMap(allRecipes);
 		allRecipes.put(1, recipeOne);
 		allRecipes.put(2, recipeTwo);
-		
-	
+		assertEquals("Mismatch on get favorites result", testObjA.getFavorites().size(), 1);
+		assertEquals("Mismatch on get Do Not Show result", testObjA.getDoNotShows().size(), 1);
+		assertEquals("Mismatch on get To Explore result", testObjA.getToExplores().size(), 1);
 	}
 
-	@Test
-	public void testGetRecipeId() {
-		
-	}
 
 	@Test
-	public void testGetRecipeMap() {
-		
+	public void testGetSetRecipeMap() {
+		RecipeAPI testObj4 = new RecipeAPI();
+		HashMap<Integer, Recipe> myHash = new HashMap<Integer, Recipe>();
+		testObj4.setRecipeMap(myHash);
+		assertEquals("Mismatch on recipe map", testObj4.getRecipeMap(), myHash);
 	}
 
 	@Test
@@ -54,7 +70,6 @@ public class RecipeAPITest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 
 }
