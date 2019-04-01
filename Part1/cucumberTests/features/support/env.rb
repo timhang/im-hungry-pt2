70ed3f6745c0eb5abc4b9925df810803a6d82136
Alex Reyes <jorgerey@usc.edu>
2019-04-01 01:02:33 -0700

    changes

--- Part1/cucumberTests/features/support/env.rb
+++ Part1/cucumberTests/features/support/env.rb
@@ -5,7 +5,7 @@ require 'rspec'
 
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>"/usr/bin/chromedriver")
+  Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>"/Users/alex/eclipse-workspace/im-hungry-pt2/Part1/cucumberTests/features/support/chromedriver")
 end
 
 Capybara.default_driver = :chrome
