cad12f3b4bd41cead35da690252dc638b7ada5a1
Alex Reyes <jorgerey@usc.edu>
2019-04-07 18:50:27 -0700

    tests

--- Part1/src/test/java/API/ChangeOrderTest.java
+++ Part1/src/test/java/API/ChangeOrderTest.java
@@ -1,31 +1,65 @@
 package API;
 
 import static org.junit.Assert.*;
+
+import java.util.HashMap;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class ChangeOrderTest {
-	@Test
+	@Before
 	public void setup() {
 		
 		// add items to favorites
 		AddToList addToList = new AddToList();
-		System.out.println("Thewse are the items we have");
+		System.out.println("These are the items we have");
 		
-		addToList.add("recipe", 3, "fav");
-		addToList.add("restaurant", 5, "fav");
+		// create restaurant
+		Restaurant restaurant1 = new Restaurant(1);
+		restaurant1.setName("1");
 
-		// get items
-		JSON json = getFavoriteItems();
+		// create a recipe
+		Recipe recipe1 = new Recipe(3);
+		recipe1.setName("3");
+
+		// add it to map
+		HashMap<Integer, Restaurant> RestaurantMap = new HashMap<Integer, Restaurant>();
+		HashMap<Integer, Recipe> RecipeMap = new HashMap<Integer, Recipe>();
+		RestaurantMap.put(1, restaurant1);
+		RecipeMap.put(3, recipe1);
+
+		// send maps to session
+		RecipeAPI.setRecipeMap(RecipeMap);
+		RestAPI.setRestaurantMap(RestaurantMap);
+
+		addToList.add("restaurant", 1, "fav");
+		addToList.add("recipe", 3, "fav");
 		
-		assertEquals(json, {3, 5, 9});
-		
+		// TODO: ensure items are in the correct order
+		assertEquals("Checking if restaurant1 is still in list", true,
+				RestAPI.getRestaurantMap().get(1).getFavorite());
+
+		assertEquals("Checking if recipe3 is still in list", true, 
+				RecipeAPI.getRecipeMap().get(3).getFavorite());		
 	}
 	
 	@Test
 	public void changeOrder() {
-		newIndex(5, 0);
-		JSON json = getFavoriteItems();
 		
-		assertEquals(json, {5,0,9});
+		assertEquals("Checking if restaurant1 is still in list", true,
+				RestAPI.getRestaurantMap().get(1).getFavorite());
+
+		assertEquals("Checking if recipe3 is still in list", true, 
+				RecipeAPI.getRecipeMap().get(3).getFavorite());
+		
+		// get indexes
+		
+		
+		// change order
+		
+		// verify index was changed
+		
 	}
+	
 }
