ce81bc48bddb0b12d86570b66d20f0454dcbe613
Alex Reyes <jorgerey@usc.edu>
2019-04-24 17:44:41 -0700

    41 grocery list tests passed

--- Part1/cucumberTests/features/support/env.rb
+++ Part1/cucumberTests/features/support/env.rb
@@ -4,6 +4,7 @@ require 'capybara/cucumber'
 require 'rspec'
 
 
+driver = ""
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>"features/support/chromedriver")
 end
